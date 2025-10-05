9bf1bb3d9f411cb1c14170f0659ffe4c23d79bd3
CeansBzh <ebriantais@orange.fr>
2025-10-05 22:25:16 +0200

    Add tray icon, tray menu and handle window display

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,8 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+mod tray;
+mod window;
+
+use tauri::{Manager, Window, WindowEvent};
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -8,7 +12,25 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            #[cfg(desktop)]
+            {
+                let app_handle = app.app_handle();
+                app_handle.plugin(tauri_plugin_positioner::init())?;
+
+                tray::create(app_handle)?;
+            }
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![greet,])
+        .on_window_event(|window: &Window, event: &WindowEvent| {
+            if let tauri::WindowEvent::CloseRequested { api, .. } = event {
+                // Minimize to tray instead of closing the window
+                let app_handle = window.app_handle();
+                window::hide_main_window(&app_handle);
+                api.prevent_close();
+            }
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src-tauri/src/tray.rs
@@ -0,0 +1,46 @@
+use tauri::{
+    menu::{Menu, MenuItem},
+    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
+    AppHandle,
+};
+
+use crate::window;
+
+pub fn create(app_handle: &AppHandle) -> Result<(), Box<dyn std::error::Error>> {
+    // Create the tray menu
+    let open_i = MenuItem::with_id(app_handle, "open", "Ouvrir", true, None::<&str>)?;
+    let quit_i = MenuItem::with_id(app_handle, "quit", "Quitter", true, None::<&str>)?;
+    let menu = Menu::with_items(app_handle, &[&open_i, &quit_i])?;
+
+    TrayIconBuilder::new()
+        .icon(app_handle.default_window_icon().unwrap().clone())
+        .on_tray_icon_event(|tray_handle, event| {
+            tauri_plugin_positioner::on_tray_event(tray_handle.app_handle(), &event);
+
+            if let TrayIconEvent::Click {
+                button: MouseButton::Left,
+                button_state: MouseButtonState::Up,
+                ..
+            } = event
+            {
+                // Handle left click on the tray icon to toggle the main window
+                window::toggle_main_window(&tray_handle.app_handle());
+            }
+        })
+        .menu(&menu)
+        .show_menu_on_left_click(false)
+        .on_menu_event(|app_handle, event| match event.id.as_ref() {
+            "quit" => {
+                window::destroy_main_window(app_handle);
+                app_handle.exit(0);
+            }
+            "open" => {
+                window::show_main_window(app_handle);
+            }
+            _ => {
+                println!("menu item {:?} not handled", event.id);
+            }
+        })
+        .build(app_handle)?;
+    Ok(())
+}
